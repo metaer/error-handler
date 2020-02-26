3daa471bcc0220264f178f3ed56ccd6496a579ec
metaer <pavelpopovw@gmail.com>
2020-02-27 01:06:22 +0300

    Change default error text

--- Resources/views/error.html.php
+++ Resources/views/error.html.php
@@ -8,13 +8,7 @@
 </head>
 <body>
 <div class="container">
-    <h1>Oops! An Error Occurred</h1>
-    <h2>The server returned a "<?= $statusCode; ?> <?= $statusText; ?>".</h2>
-
-    <p>
-        Something is broken. Please let us know what you were doing when this error occurred.
-        We will fix it as soon as possible. Sorry for any inconvenience caused.
-    </p>
+    <h1>Internal server error</h1>
 </div>
 </body>
 </html>
